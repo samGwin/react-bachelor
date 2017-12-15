a95494263b86d7b9d9bdb9361d94576010b27555
Sam <h.e._pennypacker@hotmail.com>
2017-12-15 16:28:28 -0700

    First working. Up on Firebase website

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
+import Countdown from './Clock/Clock.js';
+import ReactCountdownClock from 'react-countdown-clock';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bcountDown: "",
+      countCompleted: false,
+      unlocked: false,
+    }
+  }
+
+  componentDidMount() {
+    let d = new Date();
+    d.setFullYear(2018, 0, 4);
+    this.setState({bcountDown: d});
+  }
+
+  timerComplete() {
+    this.setState({countCompleted: true});
+  }
+
+  handlePasswordChange(newVal) {
+    if(newVal === "passwordtest") {
+      this.setState({unlocked: true})
+    }
+  }
+
   render() {
+    const { countCompleted, unlocked } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to the beginning of end of Bachelorhood</h1>
+          </header>
+          {!unlocked ? <div>
+            {!countCompleted ? <div style={{marginRight: 400}}><ReactCountdownClock
+              seconds={10}
+              color="#000"
+              alpha={0.9}
+              size={400}
+              onComplete={() => this.timerComplete()}
+            /></div>
+              :
+              <Countdown
+                date={this.state.bcountDown}
+              />}
+            {countCompleted ? <TextField
+              style={{display: 'block', margin: 'auto'}}
+              floatingLabelText="Enter Password"
+              defaultValue=""
+              onChange={(e, newVal) => this.handlePasswordChange(newVal)}
+            /> : null}
+          </div>
+            : <h1 className="App-title">You Win!!!</h1>}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
