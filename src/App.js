7bbf4aff60436688be7af99f43954afb6275524b
Sam <h.e._pennypacker@hotmail.com>
2017-12-22 15:05:48 -0700

    - All the stuff

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
+import "video-react/dist/video-react.css";
+import { Player } from 'video-react';
+import Background from './resources/image.jpg'
+import MediaQuery from 'react-responsive';
+
+function calcDiff() {
+  return (Date.parse(new Date(2017, 11, 23, 9, 0, 0, 0)) - Date.parse(new Date())) / 1000;
+}
+
 
 class App extends Component {
+
   constructor() {
     super();
     this.state = {
       bcountDown: "",
       countCompleted: false,
+      countdown1: calcDiff(),
       unlocked: false,
     }
   }
@@ -19,6 +30,12 @@ class App extends Component {
   componentDidMount() {
     let d = new Date();
     d.setFullYear(2018, 0, 4);
+    
+    //let a = new Date(2017, 11, 23, 9, 0, 0, 0);
+    //let b = new Date();
+    //console.log('a', a, b, (a - b) /1000)
+    //let c = (a - b) /1000
+    //console.log(diff);
     this.setState({bcountDown: d});
   }
 
@@ -27,26 +44,26 @@ class App extends Component {
   }
 
   handlePasswordChange(newVal) {
-    if(newVal === "passwordtest") {
+    if(newVal === "passwordtesttest") {
       this.setState({unlocked: true})
     }
   }
 
   render() {
-    const { countCompleted, unlocked } = this.state;
+    const { countCompleted, unlocked, countdown1 } = this.state;
     return (
       <MuiThemeProvider>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to the beginning of end of Bachelorhood</h1>
+            <h1 className="App-title">WELCOME TO THE BEGINNING OF THE END</h1>
           </header>
           {!unlocked ? <div>
-            {!countCompleted ? <div style={{marginRight: 400}}><ReactCountdownClock
-              seconds={10}
+            {!countCompleted ? <div style={{marginRight: 350}}><ReactCountdownClock
+              seconds={countdown1}
               color="#000"
               alpha={0.9}
-              size={400}
+              size={350}
               onComplete={() => this.timerComplete()}
             /></div>
               :
@@ -54,13 +71,22 @@ class App extends Component {
                 date={this.state.bcountDown}
               />}
             {countCompleted ? <TextField
+              type="password"
               style={{display: 'block', margin: 'auto'}}
               floatingLabelText="Enter Password"
               defaultValue=""
               onChange={(e, newVal) => this.handlePasswordChange(newVal)}
             /> : null}
           </div>
-            : <h1 className="App-title">You Win!!!</h1>}
+            : 
+              <Player>
+                <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+              </Player>}
+          <div>
+            <MediaQuery minDeviceWidth={1224}>
+              <img style={{overflow: 'hidden'}} src={Background}/>
+            </MediaQuery>
+          </div>
         </div>
       </MuiThemeProvider>
     );
